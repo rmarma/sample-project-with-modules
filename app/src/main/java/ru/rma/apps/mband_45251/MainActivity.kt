300dc3b96e44d583968197ea76fc425b8b055e84
m.a.romanov <m.a.romanov@tinkoff.ru>
2022-06-27 09:09:26 +0300

    init

---
+++ app/src/main/java/ru/rma/apps/mband_45251/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.rma.apps.mband_45251
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
